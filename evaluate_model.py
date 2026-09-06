30e1f14e0665abde89766ba9bfe6c13433840169
magocoder-yo <magoyo247@gmail.com>
2026-09-05 20:06:05 -0500

    Updated diagnostics of the model
    Displayed the classification report and confusion matrix (with the accuracy) in separate files stored in a dedicated folder.

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay, roc_auc_score, roc_curve
 
 import tensorflow as tf
 import keras
@@ -17,4 +18,17 @@ X_test = X_test[..., [0,1,2]] / 255.0
 
 pred = np.argmax(model.predict(X_test), axis=1)
 
-print(f"Accuracy Score: {accuracy_score(y_test, pred)}")
+evaluating_text = f"""Accuracy Score: {accuracy_score(y_test, pred)}
+
+Classification Report:
+{classification_report(y_test, pred)}
+"""
+
+with open("./model-diagnostics/model_diagnostic_information.txt", "w", encoding="utf-8") as file:
+    file.write(evaluating_text)
+
+ConfusionMatrixDisplay.from_predictions(y_test, pred, display_labels=["Stage 0", "Stage 1", "Stage 2", "Stage 3", "Stage 4"], cmap="Blues")
+plt.title("Confusion Matrix Display")
+plt.savefig("./model-diagnostics/confusion-matrix.png")
+
+print("Evaluation complete.")
